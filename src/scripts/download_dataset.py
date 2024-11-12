cda92e0b06d9280a170033f939a780eb35a2e1a6
ist199236 <ines.pissarra@tecnico.ulisboa.pt>
2024-11-12 13:32:12 +0100

    download_dataset

--- src/scripts/download_dataset.py
+++ src/scripts/download_dataset.py
@@ -1,22 +1,33 @@
 import os
 import tarfile
-import urllib.request
+import subprocess
 
+# Download and extract the dataset
 def download_and_extract(url, extract_path):    
+    # Get the file name from the URL
     file_name = url.split('/')[-1]
+
+    save_path = os.path.join(extract_path, file_name)
     
-    if not os.path.exists(file_name):
+    # Check if the file already exists
+    if not os.path.exists(save_path):
+        # Download the file
         print(f"Downloading {file_name}...")
-        urllib.request.urlretrieve(url, file_name)
-        print("Download complete.")
+        subprocess.run(["wget", url, "-O", save_path])
+
+        extract_data(extract_path, file_name)
+    
     else:
         print(f"{file_name} already exists.")
-    
-    if not os.path.exists(extract_path):
-        os.makedirs(extract_path)
-    
-    with tarfile.open(file_name, "r:gz") as tar:
-        tar.extractall(path=extract_path)
-        print(f"Extracted to {extract_path}")
+        extract_data(extract_path, file_name)
 
-    # Remove file we don't need ???
+
+def extract_data(path, data_dir):
+    # Check if the path exists, if not create it
+    if not os.path.exists(path):
+        os.makedirs(path)
+    
+    # Extract the data
+    print(f"Extracting {data_dir}...")
+    with tarfile.open(os.path.join(path, data_dir), "r:gz") as tar:
+        tar.extractall(path)
